41b8a9dddc6c913765e9e8f1480715291bcc8f7c
Nakashihc <fahri121yt@gmail.com>
2024-08-05 17:02:55 +0800

    initial commit

--- Assets/AutoTargetLock.cs
+++ Assets/AutoTargetLock.cs
@@ -20,7 +20,6 @@ public class AutoTargetLock : MonoBehaviour
     [SerializeField] private float JarakMax;
 
     public bool MusuhDiTargetkan;
-    private float maxAngle;
     public Transform currentTarget;
     private float mouseX;
     private float mouseY;
@@ -30,7 +29,6 @@ public class AutoTargetLock : MonoBehaviour
 
     void Start()
     {
-        maxAngle = 90f;
         kameraCinemachine.m_XAxis.m_InputAxisName = "";
         kameraCinemachine.m_YAxis.m_InputAxisName = "";
         UpdateTargetsInRange();
